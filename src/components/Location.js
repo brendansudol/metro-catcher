0ad8a10766271cebf1da3c0559d84b91ff15cbc0
brendan sudol <brendansudol@gmail.com>
2017-02-22 23:30:33 -0500

    iterate on location component

--- src/components/Location.js
+++ src/components/Location.js
@@ -4,6 +4,9 @@ import { stationLookup } from '../data/stations'
 import { haversine } from '../util'
 
 
+const MPH_RUN = 8
+const MPH_WALK = 3
+
 const Location = ({ isAvailable, isLocating, location, station }) => {
   if (!isAvailable || !location) return null
 
@@ -11,15 +14,19 @@ const Location = ({ isAvailable, isLocating, location, station }) => {
   const { lon: lon1, lat: lat1 } = location
   const { lon: lon2, lat: lat2 } = stationInfo
   const distance = haversine(lon1, lat1, lon2, lat2)
+  const minsRun = Math.round(distance / MPH_RUN * 60)
+  const minsWalk = Math.round(distance / MPH_WALK * 60)
+  const fmt = m => `~${m} minute${m !== 1 ? 's' : ''}`
 
   return (
-    <div className='mb3 p2 h6 border'>
-      Distance to metro:
-      <pre>{distance} miles</pre>
-      Your location:
-      <pre>{JSON.stringify(location, null, 2)}</pre>
-      Station location:
-      <pre>{JSON.stringify(stationInfo, null, 2)}</pre>
+    <div className='mb3 p2 h5' style={{
+      backgroundColor: '#eee',
+      borderRadius: 5
+    }}>
+      You are <strong>{distance.toFixed(1)} miles</strong> from this station.
+      You can make it there in <strong>{fmt(minsRun)}</strong> if you
+      run ({MPH_RUN}mph) or <strong>{fmt(minsWalk)}</strong> if you walk
+      ({MPH_WALK}mph). Good luck!
     </div>
   )
 }
